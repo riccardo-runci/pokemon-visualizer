37b7fe89bd0b12acfca3ff8cd05e547b993b7092
Riccardo Runci <runci@webmonks.it>
2021-10-26 00:06:37 +0200

    project setup

---
+++ pokemon-visualizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokemon-visualizer
+//
+//  Created by Riccardo Runci on 26/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
